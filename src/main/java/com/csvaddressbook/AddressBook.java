988f2dd013c05141d65bbd47ea18a9de5324d86d
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-20 18:01:51 +0530

    Add:New Contact to the Book

--- src/main/java/com/csvaddressbook/AddressBook.java
+++ src/main/java/com/csvaddressbook/AddressBook.java
@@ -1,5 +1,7 @@
 package com.csvaddressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
     private final String firstName;
     private final String lastName;
@@ -10,25 +12,48 @@ public class AddressBook {
     private final String phone;
     private final String email;
 
+    public AddressBook() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        this.firstName = scan.next();
+        System.out.println("Enter last Name");
+        this.lastName = scan.next();
+        System.out.println("Enter Address");
+        this.address = scan.next();
+        System.out.println("Enter Your City Name");
+        this.city = scan.next();
+        System.out.println("Enter Your State");
+        this.state = scan.next();
+        System.out.println("Enter Zip");
+        this.zip = scan.next();
+        System.out.println("Enter Phone Number");
+        this.phone = scan.next();
+        System.out.println("Enter Email");
+        this.email = scan.next();
+    }
 
-    public AddressBook(String firstName, String lastName, String address, String city,
-                       String state, String zip, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
-    }
     public String toString(){
-        return "First Name:"+firstName+"\nLast Name:"+lastName+"\nAddress:"+address+
-                "\nState:"+state+"\nCity:"+city+"\nZIP:"+zip+"\nPhone:"+phone+"\nEmail:"+email;
+        return "First Name:"+firstName+", Last Name:"+lastName+", Address:"+address+
+                ", State:"+state+", City:"+city+", ZIP:"+zip+", Phone:"+phone+", Email:"+email;
     }
+
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Number Of Entry U Want");
+        int noOfEntry = scan.nextInt();
 
+        String list[] = new String[noOfEntry];
+
+        for (int i=0; i<noOfEntry; i++) {
+            System.out.println("Enter Value For contact No."+(i+1));
+            AddressBook obj = new AddressBook();
+            String contactDetails = obj.toString();
+            list[i]=contactDetails;
+        }
+        for (int i=0; i<noOfEntry; i++){
+            System.out.println(list[i]);
+        }
     }
 }
 
