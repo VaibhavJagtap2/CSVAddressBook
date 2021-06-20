830fe74e589e1c5d901ff6cf559c60a9edfdbdc9
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-20 20:00:28 +0530

    Add:To Delete Contact

--- src/main/java/com/csvaddressbook/AddressBook.java
+++ src/main/java/com/csvaddressbook/AddressBook.java
@@ -3,98 +3,114 @@ package com.csvaddressbook;
 import java.util.Objects;
 import java.util.Scanner;
 
-class Book{
-    class Entry{
-        private final String firstName;
-        private final String lastName;
-        private final String address;
-        private final String city;
-        private final String state;
-        private final String zip;
-        private final String phone;
-        private final String email;
+ class Entry {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phone;
+    private final String email;
 
-        Entry(String firstName, String lastName, String address, String city, String state,
-              String zip, String phone, String email){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.phone = phone;
-            this.email = email;
-        }
-
-        Entry(){
-            this("","","","","","","","");
-        }
-
-        public String toString(){
-            return "First Name:"+firstName+"\nLast Name:"+lastName+"\nAddress:"+address+
-                    "\nState:"+state+"\nCity:"+city+"\nZIP:"+zip+"\nPhone:"+phone+"\nEmail:"+email;
-        }
+    Entry(String firstName, String lastName, String address, String city, String state, String zip, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
     }
+
+    Entry() {
+        this("", "", "", "", "", "", "", "");
+    }
+
+    public String toString() {
+        return "First Name:" + firstName + "\nLast Name:" + lastName + "\nAddress:" + address +
+                "\nState:" + state + "\nCity:" + city + "\nZIP:" + zip + "\nPhone:" + phone + "\nEmail:" + email;
+    }
+
 
     //Keeps track of how many entries are in the book
     private int entries = 0;
-    Entry[] content;
-    public void initEntries(int e){
-        content = new Entry[e];
-        for (int i = 0;i<content.length;i++){      //Initializes an array of entries, then loops through to initialize each individual entry
-            content[i] = new Entry();
+    Entry[] list;
+
+    public void initEntries(int e) {
+        list = new Entry[e];
+        for (int i = 0; i < list.length; i++) {      //Initializes an array of entries, then loops through to initialize each individual entry
+            list[i] = new Entry();
         }
     }
+
     // Returns the length
-    public int getLength(){
-        return content.length;
+    public int getLength() {
+        return list.length;
     }
+
     //Adds an entry to the book
     public void addEntry(String firstName, String lastName, String address, String city,
-                         String state, String zip, String phone, String email){
-        content[entries] = new Entry(firstName, lastName, address, city, state, zip, phone, email);
+                         String state, String zip, String phone, String email) {
+        list[entries] = new Entry(firstName, lastName, address, city, state, zip, phone, email);
         entries++;
     }
 
     //Edits the values of an entry according to the name
     public void editEntry(String firstName, String lastName, String address, String city,
-                          String state, String zip, String phone, String email, String name){
-        for (int i = 0;i<content.length;i++) {
-            if (Objects.equals(content[i].firstName, name))
-                content[i] = new Entry(firstName, lastName, address, city, state, zip, phone, email);
+                          String state, String zip, String phone, String email, String name) {
+        for (int i = 0; i < list.length; i++) {
+            if (Objects.equals(list[i].firstName, name))
+                list[i] = new Entry(firstName, lastName, address, city, state, zip, phone, email);
             else
                 System.out.println("Person Not found at Entry");
         }
     }
+
+    //Deleted an entry from the book according to the name
+    public void deleteEntry(String name) {
+        if (entries > 0) {
+            Entry[] removedArray = new Entry[list.length];
+            for (int i = 0, k = 0; i < list.length; i++) {
+                if (Objects.equals(list[i].firstName, name)) {
+                    continue;
+                }
+                removedArray[k++] = list[i];
+            }
+            System.arraycopy(removedArray, 0, list, 0, entries);
+
+        } else System.out.println("Error: book is empty.");
+    }
+
 }
 
-public class AddressBook {
+class AddressBook {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        Book book = new Book();
+        Entry entry = new Entry();
         while (true) {
-            System.out.print("How many entries in book ");
+            System.out.print("How Many Entries U Want In AddressBook ");
             int numOfEntry = sc.nextInt();
             if (numOfEntry > 0) {
-                book.initEntries(numOfEntry);
+                entry.initEntries(numOfEntry);
                 break;
             } else System.out.println("You must create at least 1 Entry.");
         }
 
-        boolean done = false;
         int choice;
         while (true) {
-            System.out.println("Book is currently selected.");
-            for (int i = 0; i < book.getLength(); i++) {
-                System.out.println("===========Entry " + (i + 1) + " ===========");
-                System.out.println(book.content[i]); //Accessing the array of entries INSIDE the array of books/the book
+            System.out.println("WelCome To AddressBook");
+            for (int i = 0; i < entry.getLength(); i++) {
+                System.out.println("===========Entry Format===========");
+                System.out.println(entry.list[i]); //Accessing the array of entries INSIDE the array of books/the book
                 System.out.println("================================");
             }
 
             System.out.println("1. Add an entry");
             System.out.println("2. Edit an entry");
-            System.out.println("3. Exit the menu");
+            System.out.println("3. Remove an entry");
+            System.out.println("4. Exit the menu");
             choice = sc.nextInt();
             String firstName, lastName, address, city, state, zip, phone, email;
             switch (choice) {
@@ -115,10 +131,10 @@ public class AddressBook {
                     phone = sc.next();
                     System.out.print("Email: ");
                     email = sc.next();
-                    book.addEntry(firstName, lastName, address, city, state, zip, phone, email);
+                    entry.addEntry(firstName, lastName, address, city, state, zip, phone, email);
                     break;
                 case 2:
-                    System.out.print("Please enter the first name of Person You want to edit: ");
+                    System.out.print("Please enter the first name of Person u want to edit: ");
                     String name = sc.next();
                     System.out.print("First name: ");
                     firstName = sc.next();
@@ -136,12 +152,15 @@ public class AddressBook {
                     phone = sc.next();
                     System.out.print("Email: ");
                     email = sc.next();
-                    book.editEntry(firstName, lastName, address, city,
-                            state, zip, phone, email, name);
+                    entry.editEntry(firstName, lastName, address, city, state, zip, phone, email, name);
+                    break;
+                case 3:
+                    System.out.println("Please enter the first name of Person u want to delete: ");
+                    name = sc.next();
+                    entry.deleteEntry(name);
                     break;
                 default:
                     System.out.print("Invalid Choice");
-                    break;
             }
         }
     }
