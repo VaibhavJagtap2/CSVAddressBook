1efbd8d3043215bcb48c76d91f04f26cea9dea89
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-20 17:49:29 +0530

    Add:Create Contacts in Book

--- src/main/java/com/csvaddressbook/AddressBook.java
+++ src/main/java/com/csvaddressbook/AddressBook.java
@@ -1,9 +1,34 @@
 package com.csvaddressbook;
 
 public class AddressBook {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phone;
+    private final String email;
+
+
+    public AddressBook(String firstName, String lastName, String address, String city,
+                       String state, String zip, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+    public String toString(){
+        return "First Name:"+firstName+"\nLast Name:"+lastName+"\nAddress:"+address+
+                "\nState:"+state+"\nCity:"+city+"\nZIP:"+zip+"\nPhone:"+phone+"\nEmail:"+email;
+    }
 
     public static void main(String[] args) {
-    System.out.println("Welcome TO CSVAddressBook");
+
     }
 }
 
